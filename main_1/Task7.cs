d813cd4dc8a777e87ebc67bc2694aa3039d105f0
@viktor_kornienk <v1ctor.c4ernov@gmail.com>
2026-07-02 00:01:02 +0300

    fix task (part 2)

--- main_1/Task7.cs
+++ main_1/Task7.cs
@@ -9,12 +9,20 @@ namespace main_1
         {
             uint countPeople = 0;
             uint timeMinuteWait = 10;
-            uint TimeTotalWait = 0;
+            uint timeTotalWait = 0;
+            uint constTimeSecondMinute = 60;
+            uint hours = 0;
+            uint minutes = 0;
 
             Console.WriteLine("Сколько людей в очереди?");
             countPeople = Convert.ToUInt32(Console.ReadLine());
-            TimeTotalWait = countPeople * timeMinuteWait;
-            Console.WriteLine($"Вы должны отстоять в очереди {TimeTotalWait / 60} часа и {TimeTotalWait % 60} минут.");
+
+            timeTotalWait = countPeople * timeMinuteWait;
+            hours = timeTotalWait / constTimeSecondMinute;
+            minutes = timeTotalWait % constTimeSecondMinute;
+
+            Console.WriteLine($"Вы должны отстоять в очереди {hours} " +
+                $"часа и {minutes} минут.");
 
         }
     }
