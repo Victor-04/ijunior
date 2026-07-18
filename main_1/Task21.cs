952a425b8feface6a93dc49dbf91b8c5c396afca
@viktor_kornienk <v1ctor.c4ernov@gmail.com>
2026-07-18 20:47:17 +0300

    start `For` block

---
+++ main_1/Task21.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace main_1
+{
+    public class Task21 : ITask
+    {
+        public void Run()
+        {
+            string textRetry = "";
+            uint countRetry = 0;
+
+            Console.WriteLine("Что вы хотите повторить?");
+            textRetry = Console.ReadLine();
+
+            Console.WriteLine("Сколько раз?");
+            countRetry = Convert.ToUInt32(Console.ReadLine());
+
+            for (int i = 0; i < countRetry; i++)
+            {
+                Console.WriteLine(textRetry);
+            }
+
+        }
+    }
+}
