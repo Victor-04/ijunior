dd5774fb707209a06c62d0d1d6440c922358e30d
@viktor_kornienk <v1ctor.c4ernov@gmail.com>
2026-07-20 08:43:40 +0300

    continue

---
+++ main_1/Task22.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace main_1
+{
+    public class Task22 : ITask
+    {
+        public void Run()
+        {
+            int totalSum = 0;
+            int interSum = 0;
+            int firstItem = 0;
+            int secondItem = 0;
+            uint countIteration = 0;
+            string inputText = "";
+
+            Console.WriteLine("Калькулятор сложения двух чисел. (для выхода введите 'exit')");
+            while (true)
+            {
+                Console.WriteLine("Введите первое слогаемое?");
+                inputText = Console.ReadLine();
+                if (inputText == "exit")
+                {
+                    break;
+                }
+                firstItem = Convert.ToInt32(inputText);
+
+                Console.WriteLine("Введите второе слогаемое?");
+                inputText = Console.ReadLine();
+                if (inputText == "exit")
+                {
+                    break;
+                }
+                secondItem = Convert.ToInt32(inputText);
+
+                interSum = firstItem + secondItem;
+                Console.WriteLine($"Результат: {interSum}");
+
+                totalSum += interSum;
+                countIteration++;
+            }
+
+            Console.WriteLine($"Выход из цикла. Count loop: {countIteration}. Total sum by operations: {totalSum}");
+
+        }
+    }
+}
