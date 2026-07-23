5cf41efaf4441e9fd5230aa9be670f731b11bc83
@viktor_kornienk <v1ctor.c4ernov@gmail.com>
2026-07-23 09:19:02 +0300

    continue

---
+++ main_1/Task23.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace main_1
+{
+    public class Task23 : ITask
+    {
+        public void Run()
+        {
+
+            int initValue = 5;
+            int finalValue = 103;
+            int stepValue = 7;
+            int countIterations = 0;
+
+            countIterations = (finalValue - initValue) / stepValue;
+
+            for (int i = initValue; i <= finalValue; i += stepValue)
+            {
+                Console.Write($"{i} ");
+            }
+        }
+    }
+}
---
+++ main_1/Task24.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+
+namespace main_1
+{
+    public class Task24 : ITask
+    {
+        public void Run()
+        {
+            var random = new Random();
+
+            int targetValue = 0;
+            int maxBorder = 100;
+            int firstСondition = 3; // эффективнее такие данные держать в массиве (но мы их еще не проходили :) )
+            int secondCondition = 5;
+            int totalSum = 0;
+
+            string checkLine = "";
+
+            targetValue = random.Next(100);
+            Console.WriteLine($"Загаданное число: {targetValue}");
+
+            for (int i = 0; i <= targetValue; i++)
+            {
+                if (i % firstСondition == 0 || i % secondCondition == 0)
+                {
+                    checkLine += $"{i} ";
+                    totalSum += i;
+                }
+            }
+
+            Console.WriteLine($"Сумма значений: {totalSum}");
+            Console.WriteLine($"Строка для проверки: {checkLine}");
+        }
+    }
+}
