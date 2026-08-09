64a9c074c46e69bfab5ea461c202911414fdf2cd
V_Red <041199kvc@gmail.com>
2026-08-09 14:37:53 +0300

    continue

---
+++ main_1/Task28.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Windows.Input;
+
+namespace main_1
+{
+    public class Task28 : ITask
+    {
+
+        public void Run()
+        {
+            string password = "12345";
+            string secretLine = "secret line";
+
+            string inputLine = "";
+            int attempt = 0;
+
+            while (attempt < 3) 
+            {
+                Console.Write("Введите пароль: ");
+                inputLine = Console.ReadLine();
+
+                if (inputLine == password)
+                {
+                    Console.Write($"secretLine: {secretLine}");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Ошибка, попробуйте снова");
+                }
+                attempt++;
+            }
+        }
+    }
+}
