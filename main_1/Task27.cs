860edbf7ae8819df0b5bf17f64446d771f6d86ab
V_Red <041199kvc@gmail.com>
2026-08-09 13:44:46 +0300

    continue

---
+++ main_1/Task27.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Windows.Input;
+
+namespace main_1
+{
+    public class Task27 : ITask
+    {
+
+        public void Run()
+        {
+            char symbol = '-';
+            int lengthLine = 0;
+            string inputLine = "";
+
+            Console.Write("Ввидите символ: ");
+            inputLine = Console.ReadLine();
+
+            if (inputLine.Length > 1)
+            {
+                Console.Write("Вы ввели больше одного символа!\nПовторите попытку и введите один символ!\n");
+                return;
+            }
+
+            symbol = inputLine[0];
+
+            Console.Write("Ввидите Имя: ");
+            inputLine = Console.ReadLine();
+
+            inputLine = symbol + inputLine + symbol;
+            lengthLine = inputLine.Length;
+
+            Console.WriteLine();
+            for (int i = 0; i < lengthLine; i++)
+            {
+                Console.Write(symbol);
+            }
+            Console.WriteLine();
+
+            Console.WriteLine(inputLine);
+
+            for (int i = 0; i < lengthLine; i++)
+            {
+                Console.Write(symbol);
+            }
+            Console.WriteLine();
+        }
+    }
+}
