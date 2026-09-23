13d30cd649e3c6760e787a8810a3c95e9e3303e4
v1ctor.c4ernov <v1ctor.c4ernov@gmail.com>
2026-09-23 23:43:38 +0300

    37

---
+++ main_1/Task37.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace main_1
+{
+    public class Task37 : ITask
+    {
+        public void Run()
+        {
+            string row = "Duis aute irure " +
+                "dolor in reprehenderit in voluptate, " +
+                "nam libero tempore, cum soluta nobis est " +
+                "eligendi optio, cumque nihil impedit, quo minus " +
+                "id, quod maxime placeat, facere possimus, omnis " +
+                "voluptas assumenda est, omnis dolor repellendus.";
+
+            Console.WriteLine($"{row}\n");
+
+            string[] words = row.Split();
+            Console.WriteLine($"words.Length: {words.Length}");
+
+            for (int i = 0; i < words.Length; i++)
+            {
+                Console.WriteLine($"[{i}]: {words[i]}");
+            }
+        }
+    }
+}
