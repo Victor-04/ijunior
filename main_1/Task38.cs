5b76471a6626acb464599f8c6e8e7bd6301b784b
v1ctor.c4ernov <v1ctor.c4ernov@gmail.com>
2026-09-25 18:29:12 +0300

    fix 38

--- main_1/Task38.cs
+++ main_1/Task38.cs
@@ -17,18 +17,23 @@ namespace main_1
             Console.Write("\nВведите смещение: ");
 
             int shifts = Convert.ToInt32(Console.ReadLine());
+            int arreySize = array.Length;
 
-            if (array.Length > 0)
+            if (arreySize > 0)
             {
+                if (shifts >= arreySize)
+                {
+                    shifts = shifts % arreySize;
+                }
                 for (int s = 1; s <= shifts; s++)
                 {
                     int lastElement = array[0];
 
-                    for (int i = 0; i < array.Length - 1; i++)
+                    for (int i = 0; i < arreySize - 1; i++)
                     {
                         array[i] = array[i + 1];
                     }
-                    array[array.Length - 1] = lastElement;
+                    array[arreySize - 1] = lastElement;
                 }
             }    
 
