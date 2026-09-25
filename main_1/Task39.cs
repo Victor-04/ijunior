13e7bc9553cf20765da57671794f60d5a2b1761b
v1ctor.c4ernov <v1ctor.c4ernov@gmail.com>
2026-09-25 19:37:53 +0300

    39

---
+++ main_1/Task39.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace main_1
+{
+    public class Task39 : ITask
+    {
+        public void Run()
+        {
+            const char InputGlyph = '(';
+            const char OutputGlyph = ')';
+
+            string inputLine = "";
+            int counterIn = 0;
+            int maxIn = 0;
+
+            Console.Write("\nВведите строку: ");
+            inputLine = Console.ReadLine();
+
+            if (inputLine.Length == 0)
+                inputLine = "(()(()))"; 
+
+            for (int i = 0; i < inputLine.Length; i++)
+            {
+                switch (inputLine[i])
+                {
+                    case InputGlyph:
+                        counterIn++;
+                        break;
+
+                    case OutputGlyph:
+                        counterIn--;
+                        break;
+
+                    default:
+                        break;
+                }
+
+                if (counterIn > maxIn)
+                    maxIn = counterIn;
+
+                if (counterIn < 0)
+                    break;
+            }
+
+            Console.WriteLine();
+
+            if (counterIn == 0)
+                Console.WriteLine("Status: OK");
+            else
+                Console.WriteLine("Status: ERROR");
+
+            Console.WriteLine($"Max deep: {maxIn}");
+            Console.WriteLine($"[DEBUG] counterIn: {counterIn}");
+        }
+    }
+}
